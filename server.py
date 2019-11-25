85bd58c7060a5c67f1d0fa3ccd54e4a1c017203e
Werozel <khaibulaev.mn@gmail.com>
2019-11-25 14:07:12 +0300

    Работает переадрессация с index на login

--- server.py
+++ server.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 
 @app.route("/", methods=["POST", "GET"])
 def index():
+    print(request.values)
     if request.method == "POST":
         return redirect(url_for("login"))
     else:
@@ -13,6 +14,7 @@ def index():
 
 @app.route("/login", methods=["POST", "GET"])
 def login():
+    print(request.values)
 
     return app.send_static_file("login.html")
 
