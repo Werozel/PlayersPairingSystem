9a492c1f4d8bbd9b3d43d396e590077fea37bc78
Werozel <khaibulaev.mn@gmail.com>
2019-11-27 11:50:02 +0300

    Начал делать формы

--- server.py
+++ server.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/index")
 def index():
-    return render_template("index.html", title="Main Page")
+    return render_template("index.html", title="Main Page", sidebar=True)
 
 
 @app.route("/login")
