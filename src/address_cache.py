fdbad8a2b33a0cd0d5f4cc7fbd27313f5fa2eb97
Werozel <khaibulaev.mn@gmail.com>
2020-10-04 23:02:56 +0300

    Added js request to api

--- src/address_cache.py
+++ src/address_cache.py
@@ -16,4 +16,4 @@ address_cache: dict = load_address_cache()
 def save_address_cache():
     with open("address_cache.py", "wb") as out:
         pickle.dump(address_cache, out)
-    print("Caches saved")
+    print(f"Caches saved, {address_cache}")
